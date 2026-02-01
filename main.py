2d559b89d2d0616544d442348647041785140c65
Dilorom <diloromashirova709@gmail.com>
2026-02-01 23:27:49 +0500

    hm-08: 15 varibles one print

---
+++ main.py
@@ -0,0 +1,56 @@
+# print() console ga malumot chiqaradi
+# print("dilorom")
+# print([1,4,6,7,8])
+# print("Malumotlarni kiritamiz")
+
+# x = 12
+# print(x)
+# dilorom = "Dilrom"
+# print("dilorom")
+
+age = 20
+print(age)
+
+name = "Dilorom"
+print(name)
+
+surname = "Ashirova"
+print(surname)
+
+height = 150
+print(height)
+
+weight = 49
+print(weight)
+
+status = "ok"
+print(status)
+
+result = "Passed"
+print(result)
+
+score = 95
+print(score)
+
+price = 12000
+print(price)
+
+count = 3
+print(count)
+
+total = 36000
+print(total)
+
+summa = 100
+print(summa)
+
+average = 85.5
+print(average)
+
+level = 1
+print(1)
+
+is_active = True
+print(is_active)
+
+print(age, name, surname, score, price, count, total, summa, average, height, weight, is_active, result, level, status)
